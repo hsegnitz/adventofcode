2a3dae39e882c8bb5ec4cd29941be2870618913b
Holger Segnitz <hsegnitz@fastbill.com>
2018-12-21 01:48:07 +0100

    paint the small one!

--- 20/1.php
+++ 20/1.php
@@ -223,7 +223,7 @@ class room
             case 'S':
                 if (! $this->getSouth() instanceof room) {
                     $nextRoom = new room($this->x, $this->y + 1);
-                    $this->setNorth($nextRoom);
+                    $this->setSouth($nextRoom);
                 }
                 $this->getSouth()->walk(substr($in, 1));
                 break;
@@ -234,13 +234,61 @@ class room
                 }
                 $this->getWest()->walk(substr($in, 1));
                 break;
+            case '(':
+                $posClosingBrace = $this->findClosingParanthesis($in);
+                $branches = $this->splitWithBraces(substr($in, 1, $posClosingBrace-1)); // FIXME: leave (...|...) intact ;)
+                foreach ($branches as $branch) {
+                    $this->walk($branch . substr($in, $posClosingBrace+1));
+                }
+                break;
             default:
                 throw new RuntimeException('Go fuck yourself with your ' . $in[0]);
         }
     }
+
+    /**
+     * @param  string   $in
+     * @return string[]
+     */
+    private function splitWithBraces($in)
+    {
+        $pattern = '/\(([^\(\)]+)\)/';
+        while (true) {
+            $newIn = preg_replace_callback($pattern, function ($matches) {return '[' . str_replace('|', '-', $matches[1]) . ']';}, $in);
+            if ($newIn === $in) {
+                break;
+            }
+            $in = $newIn;
+        }
+
+        $out = [];
+        foreach (explode('|', $newIn) as $row) {
+            $out[] = str_replace(['[', '-', ']'], ['(', '|', ')'], $row);
+        }
+
+        return $out;
+    }
+
+    private function findClosingParanthesis($in)
+    {
+        $count = 0;
+        for ($i = 0; $i < strlen($in); $i++) {
+            if ($in[$i] === '(') {
+                ++$count;
+            } elseif ($in[$i] === ')') {
+                --$count;
+            }
+
+            if ($count === 0) {
+                return $i;
+            }
+        }
+    }
+
+
 }
 
-$in = file_get_contents('tiny.txt');
+$in = file_get_contents('small.txt');
 
 $root = new room(0 ,0);
 $root->walk(substr($in, 1));
