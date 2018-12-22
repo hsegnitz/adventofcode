7252c3b1015d104b554e4927537c931d1c0569d6
Holger Segnitz <hsegnitz@fastbill.com>
2018-12-22 19:37:32 +0100

    Solution for day 20.1 without building the map but it sill seems to run for ages...

--- 20/1.php
+++ 20/1.php
@@ -4,270 +4,62 @@ ini_set('xdebug.max_nesting_level', 512);
 
 class room
 {
-    private static $instanceCount = 0;
-    private static $minX = 0;
-    private static $minY = 0;
-    private static $maxX = 0;
-    private static $maxY = 0;
-
-    /** @var room[][] */
-    private static $map  = [];
-
-    private $x;
-
-    private $y;
-
-    /** @var int */
-    private $id;
-
-    /** @var room */
-    private $north;
-
-    /** @var room */
-    private $east;
-
-    /** @var room */
-    private $south;
-
-    /** @var room */
-    private $west;
+    /** @var int[] */
+    private $seen = [];
 
     /**
-     * room constructor.
+     * Whenever we reach a tile we have seen already, we check if we had a shorter way to it before.
+     * If so, we return the short distance and continue counting from there. If the new path is
+     * short than the previous one, we update the value and return it.
      *
-     * @param $x
-     * @param $y
-     */
-    public function __construct($x, $y)
-    {
-        $this->id = self::$instanceCount++;
-        $this->x = $x;
-        $this->y = $y;
-
-        if (isset(self::$map[$y])) {
-            if (isset(self::$map[$y][$x])) {
-                throw new RuntimeException('trying to recreate a room -- this is not the matrix!');
-            }
-            self::$map[$y][$x] = $this;
-        } else {
-            self::$map[$y] = [
-                $x => $this,
-            ];
-        }
-
-        self::$maxX = max(self::$maxX, $this->x);
-        self::$maxY = max(self::$maxY, $this->y);
-        self::$minX = min(self::$minX, $this->x);
-        self::$minY = min(self::$minY, $this->y);
-    }
-
-    /**
+     * This way we eliminate "immediate continue branches", short loops "WESN" and even longer loops
+     *
+     * @param  int $x
+     * @param  int $y
+     * @param  int $distance
      * @return int
      */
-    public function getId()
+    private function registerAsSeen($x, $y, $distance)
     {
-        return $this->id;
-    }
-
-    /**
-     * @param     $x
-     * @param int $y
-     * @return room
-     */
-    private static function getOrCreateUnrelated($x, int $y): room
-    {
-        if (isset(self::$map[$y][$x])) {
-            $nextRoom = self::$map[$y][$x];
-        } else {
-            $nextRoom = new room($x, $y);
+        $key = "{$x}x{$y}";
+        if (! isset($this->seen[$key])) {
+            $this->seen[$key] = $distance;
         }
 
-        return $nextRoom;
-    }
-
-    /**
-     * @return room
-     */
-    public function getNorth()
-    {
-        return $this->north;
-    }
-
-    /**
-     * @param room $north
-     */
-    public function setNorth($north)
-    {
-        $this->north = $north;
-        if ($north->getSouth() instanceof room) {
-            return;
-        }
-        $north->setSouth($this);
-    }
-
-    /**
-     * @return room
-     */
-    public function getEast()
-    {
-        return $this->east;
-    }
-
-    /**
-     * @param room $east
-     */
-    public function setEast($east)
-    {
-        $this->east = $east;
-        if ($east->getWest() instanceof room) {
-            return;
-        }
-        $east->setWest($this);
-    }
-
-    /**
-     * @return room
-     */
-    public function getSouth()
-    {
-        return $this->south;
-    }
-
-    /**
-     * @param room $south
-     */
-    public function setSouth($south)
-    {
-        $this->south = $south;
-        if ($south->getNorth() instanceof room) {
-            return;
-        }
-        $south->setNorth($this);
-    }
-
-    /**
-     * @return room
-     */
-    public function getWest()
-    {
-        return $this->west;
-    }
-
-    /**
-     * @param room $west
-     */
-    public function setWest($west)
-    {
-        $this->west = $west;
-        if ($west->getEast() instanceof room) {
-            return;
-        }
-        $west->setEast($this);
-    }
-
-    public static function printMap()
-    {
-        for ($y = self::$minY; $y <= self::$maxY; $y++) {
-            // above -- only if first row
-            if ($y === self::$minY) {
-                for ($x = self::$minX; $x <= self::$maxX; $x++) {
-                    if ($x === self::$minX) {
-                        echo '#';
-                    }
-                    echo '##';
-                }
-                echo "\n";
-            }
-
-            // room line
-            for ($x = self::$minX; $x <= self::$maxX; $x++) {
-                if ($x === self::$minX) {
-                    echo '#';
-                }
-
-                if (isset(self::$map[$y][$x]) && self::$map[$y][$x] instanceof room) {
-                    echo self::$map[$y][$x]->getId() === 0 ? 'X' : '.';
-                } else {
-                    echo '?';
-                }
-
-                if (isset(self::$map[$y][$x]) && self::$map[$y][$x]->getEast() instanceof room) {
-                    echo '|';
-                } else {
-                    echo '#';
-                }
-            }
-            echo "\n";
-
-            // below
-            for ($x = self::$minX; $x <= self::$maxX; $x++) {
-                if ($x === self::$minX) {
-                    echo '#';
-                }
-
-                if (isset(self::$map[$y][$x]) && self::$map[$y][$x]->getSouth() instanceof room) {
-                    echo '-';
-                } else {
-                    echo '#';
-                }
-                echo '#';
-            }
-            echo "\n";
-        }
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getX()
-    {
-        return $this->x;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getY()
-    {
-        return $this->y;
+        $this->seen[$key] = min($distance, $this->seen[$key]);
+        return $this->seen[$key];
     }
 
     /**
      * @param string $in
      */
-    public function walk($in)
+    public function walk($x, $y, $dist, $in)
     {
-        $currentRoom = $this;
-        for ($i = 0; $i < strlen($in); $i++) {
+        $dist = $this->registerAsSeen($x, $y, $dist);
+
+        $len = strlen($in);
+        for ($i = 0; $i < $len; $i++) {
             switch ($in[$i]) {
                 case '$':
                     return;
                 case 'N':
-                    $x = $currentRoom->getX();
-                    $y = $currentRoom->getY() - 1;
-                    $currentRoom = self::getOrCreateUnrelated($x, $y);
+                    $dist = $this->registerAsSeen($x, --$y, ++$dist);
                     break;
                 case 'E':
-                    $x = $currentRoom->getX() + 1;
-                    $y = $currentRoom->getY();
-                    $currentRoom = self::getOrCreateUnrelated($x, $y);
+                    $dist = $this->registerAsSeen(++$x, $y, ++$dist);
                     break;
                 case 'S':
-                    $x = $currentRoom->getX();
-                    $y = $currentRoom->getY() + 1;
-                    $currentRoom = self::getOrCreateUnrelated($x, $y);
+                    $dist = $this->registerAsSeen($x, ++$y, ++$dist);
                     break;
                 case 'W':
-                    $x = $currentRoom->getX() - 1;
-                    $y = $currentRoom->getY();
-                    $currentRoom = self::getOrCreateUnrelated($x, $y);
+                    $dist = $this->registerAsSeen(--$x, $y, ++$dist);
                     break;
                 case '(':
                     $remainder = substr($in, $i);
-                    $posClosingBrace = $currentRoom->findClosingParanthesis($remainder);
-                    $branches = $currentRoom->splitWithBraces(substr($remainder, 1, $posClosingBrace-1));
+                    $posClosingBrace = $this->findClosingParanthesis($remainder);
+                    $branches = $this->splitWithBraces(substr($remainder, 1, $posClosingBrace-1));
                     foreach ($branches as $branch) {
-                        $currentRoom->walk($branch . substr($remainder, $posClosingBrace+1));
+                        $this->walk($x, $y, $dist, $branch . substr($remainder, $posClosingBrace+1));
                     }
                     return;
                 default:
@@ -299,10 +91,15 @@ class room
         return $out;
     }
 
+    /**
+     * @param  string $in
+     * @return int
+     */
     private function findClosingParanthesis($in)
     {
         $count = 0;
-        for ($i = 0; $i < strlen($in); $i++) {
+        $len = strlen($in);
+        for ($i = 0; $i < $len; $i++) {
             if ($in[$i] === '(') {
                 ++$count;
             } elseif ($in[$i] === ')') {
@@ -314,14 +111,19 @@ class room
             }
         }
     }
+
+    public function getSeen()
+    {
+        return $this->seen;
+    }
 }
 
-$in = file_get_contents('in.txt');
+$in = file_get_contents('large.txt');
 
-$root = new room(0 ,0);
-$root->walk(substr($in, 1));
+$root = new room();
+$root->walk(0, 0, 0, substr($in, 1));
+print_r($root->getSeen());
 
-room::printMap();
 
 
 // solution:
