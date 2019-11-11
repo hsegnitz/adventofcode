e3fda4cf3d40fa467d2f12a454b3331506829470
Holger Segnitz <h.segnitz@busylamp.com>
2019-11-11 18:20:01 +0100

    2015.09.1 -- more intermediate code for way home Oo

--- java/src/main/java/y2015/d09/Part1.java
+++ java/src/main/java/y2015/d09/Part1.java
@@ -1,14 +1,11 @@
 package y2015.d09;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class Part1 {
 
-    private static HashMap<String, Boolean> cities = new HashMap<>();
+    private static String[] cities;
     private static HashMap<String, Integer> distances = new HashMap<>();
 
     public static void main(String[] args) {
@@ -24,17 +21,21 @@ public class Part1 {
 
         File file = new File("src/main/java/y2015/d09/small.txt");
         String rawLine   = "";
+        HashMap<String, Boolean> localCities = new HashMap<>();
         try {
             Scanner scanner = new Scanner(file);
             while (scanner.hasNextLine()) {
                 rawLine = scanner.nextLine();
                 String[] split = rawLine.split(" ");
-                cities.put(split[0], true);
-                cities.put(split[2], true);
+                localCities.put(split[0], true);
+                localCities.put(split[2], true);
 
                 distances.put(split[0] + ":" + split[2], Integer.parseInt(split[4]));
                 distances.put(split[2] + ":" + split[0], Integer.parseInt(split[4]));
             }
+
+            Set<String> keys = localCities.keySet();
+            cities = keys.toArray(new String[0]);
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return;
