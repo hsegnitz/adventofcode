5d3c5adbb80bdbd727a088e56b3c74aab8797ee2
Holger Segnitz <holger@segnitz.net>
2019-11-16 15:33:27 +0100

    21015.13.1b (solving the example)

--- java/src/main/java/y2015/d13/Part1.java
+++ java/src/main/java/y2015/d13/Part1.java
@@ -10,12 +10,45 @@ public class Part1 {
 
     private static String[] people;
     private static HashMap<String, Integer> happinessDeltas = new HashMap<>();
-    private static int shortest = Integer.MAX_VALUE;
-    private static int longest  = Integer.MIN_VALUE;
+    private static int highest = Integer.MIN_VALUE;
 
     public static void main(String[] args) {
 
         readFile();
+
+        // nested loops - one per person
+        ArrayList<String> order = new ArrayList<>();
+        addPerson(order);
+    }
+
+    public static int calculateHappiness(ArrayList<String> order) {
+        int happiness = 0;
+        for (int i = 0; i < order.size()-1; i++) {
+            happiness += happinessDeltas.get(order.get(i)   + ":" + order.get(i+1));
+            happiness += happinessDeltas.get(order.get(i+1) + ":" + order.get(i));
+        }
+        happiness += happinessDeltas.get(order.get(0)              + ":" + order.get(order.size()-1));
+        happiness += happinessDeltas.get(order.get(order.size()-1) + ":" + order.get(0));
+        return happiness;
+    }
+
+    public static void addPerson(ArrayList<String> order) {
+        for (String person : people) {
+            ArrayList<String>orderB = (ArrayList<String>) order.clone();
+            if (order.contains(person)) {
+                continue;
+            }
+            orderB.add(person);
+            if (orderB.size() == people.length) {
+                int happiness = calculateHappiness(orderB);
+
+                highest = Math.max(highest,  happiness);
+
+                System.out.println(orderB + " -- [" + happiness + "] -- highest(" + highest + ")");
+            } else {
+                addPerson(orderB);
+            }
+        }
     }
 
     public static void readFile() {
