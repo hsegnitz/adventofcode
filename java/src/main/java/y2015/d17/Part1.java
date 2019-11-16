01d2bd28878985f20cd8134f82e0c172a8593186
Holger Segnitz <holger@segnitz.net>
2019-11-16 19:45:42 +0100

    2015.17.1 in under one second :o

--- java/src/main/java/y2015/d17/Part1.java
+++ java/src/main/java/y2015/d17/Part1.java
@@ -1,94 +1,60 @@
 package y2015.d17;
 
+import javax.swing.*;
 import java.io.File;
 import java.util.*;
 
 public class Part1 {
-
-    private static int target = 150;
+    // private static int target = 25;  // small
+    private static int target = 150;    // in
     private static int count  =   0;
-    private static HashSet<String> seen = new HashSet<>();
-    private static LinkedHashMap<Integer, Integer> allContainers = new LinkedHashMap<>();
+    private static ArrayList<Integer> allContainers = new ArrayList<>();
+
 
     public static void main(String[] args) {
+        // allContainers = small();
         allContainers = in();
 
-        calc(new LinkedHashMap<Integer, Integer>(), 0);
-
+        calc(0, 0);
         System.out.println(count);
     }
 
-    public static void calc(LinkedHashMap<Integer, Integer> previous, Integer depth) {
-        int sum = 0;
-        ArrayList<Integer> keyList = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> entry: previous.entrySet()) {
-            keyList.add(entry.getKey());
-            sum += entry.getValue();
-        }
-
-        if (sum == target) {
-            keyList.sort(Comparator.comparingInt(o -> o));
-
-            String key = Arrays.toString(keyList.toArray());
-
-            if (!seen.contains(key)) {
-                count++;
-                seen.add(key);
-                System.out.println(key);
+    public static void calc(int sum, int index) {
+        for (int i = index; i < allContainers.size(); i++) {
+            int newSum = sum + allContainers.get(i);
+            if (newSum < target) {
+                calc(newSum, i + 1);
+            } else if (newSum == target) {
+                ++count;
             }
-            return;
-        }
-
-        if (sum > target) {
-            return;
-        }
-
-        if (depth >= allContainers.size()) {
-            return;
-        }
-
-        LinkedHashMap<Integer, Integer> clone = (LinkedHashMap<Integer, Integer>) previous.clone();
-        calc(clone, depth + 1);
-
-        for (int i = depth; i < allContainers.size(); i++ ) {
-            clone = (LinkedHashMap<Integer, Integer>) previous.clone();
-            clone.put(i, allContainers.get(i));
-            calc(clone, depth + 1);
         }
     }
 
+    private static ArrayList<Integer> small() {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(20);
+        list.add(15);
+        list.add(10);
+        list.add( 5);
+        list.add( 5);
 
-    private static LinkedHashMap<Integer, Integer> small() {
-        LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>();
-        map.put(0, 20);
-        map.put(1, 15);
-        map.put(2, 10);
-        map.put(3,  5);
-        map.put(4,  5);
-
-        return map;
+        return list;
     }
 
-    private static LinkedHashMap<Integer, Integer> in() {
-        LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>();
-        int counter = 0;
+    private static ArrayList<Integer> in() {
+        ArrayList<Integer> list = new ArrayList<>();
         File file = new File("src/main/java/y2015/d17/in.txt");
         try {
             Scanner scanner = new Scanner(file);
             while (scanner.hasNextLine()) {
-                map.put(counter++, scanner.nextInt());
+                list.add(scanner.nextInt());
                 scanner.nextLine();
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-/*
-        System.out.println(map);
-        System.exit(42);
-*/
-
-        return map;
+        return list;
     }
 
 }
