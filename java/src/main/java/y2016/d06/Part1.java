67feb88b75751d9ae70c8484da7b57c2edc43558
Holger Segnitz <holger@segnitz.net>
2019-11-22 00:36:26 +0100

    2016.06.1 -- steps

---
+++ java/src/main/java/y2016/d06/Part1.java
@@ -0,0 +1,19 @@
+package y2016.d06;
+
+import common.Files;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Part1 {
+
+    public static void main(String[] args) {
+        ArrayList<HashMap<String, Integer>> columns = new ArrayList<HashMap<String, Integer>>();
+
+        String[] lines = Files.readByLines("src/main/java/y2016/d06/in.txt");
+
+        
+
+    }
+
+}
