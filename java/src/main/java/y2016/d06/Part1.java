279b26aac945040201a2e9f70efff2d20ae92834
Holger Segnitz <holger@segnitz.net>
2019-11-22 14:01:08 +0100

    2016.06.1 - small

--- java/src/main/java/y2016/d06/Part1.java
+++ java/src/main/java/y2016/d06/Part1.java
@@ -2,18 +2,45 @@ package y2016.d06;
 
 import common.Files;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Part1 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         ArrayList<HashMap<String, Integer>> columns = new ArrayList<HashMap<String, Integer>>();
 
-        String[] lines = Files.readByLines("src/main/java/y2016/d06/in.txt");
+        for (int i = 0; i < 8; i++) {
+            columns.add(new HashMap<String, Integer>());
+        }
 
-        
+        ArrayList<String> lines = Files.readByLines("src/main/java/y2016/d06/small.txt");
 
+        for (String s: lines) {
+            for (int i = 0; i < s.length(); i++) {
+                String l = s.substring(i, i+1);
+                if (!columns.get(i).containsKey(l)) {
+                    columns.get(i).put(l, 1);
+                } else {
+                    columns.get(i).put(l, columns.get(i).get(l) + 1);
+                }
+            }
+        }
+
+        for (HashMap<String, Integer> col: columns) {
+            int largest = 0;
+            for (Map.Entry<String, Integer> set: col.entrySet()) {
+                largest = Math.max(set.getValue(), largest);
+            }
+            for (Map.Entry<String, Integer> set: col.entrySet()) {
+                if (set.getValue() == largest) {
+                    System.out.print(set.getKey());
+                }
+            }
+            System.out.println("");
+        }
     }
-
 }
