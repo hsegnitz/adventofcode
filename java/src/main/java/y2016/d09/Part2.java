d344635615b5d5826d095607cd83b44b0f29e1eb
Holger Segnitz <holger@segnitz.net>
2019-11-24 18:00:34 +0100

    2016.09.2 -- well, after 20 minutes on an i9 9900K no solution, I'd say this is not working

---
+++ java/src/main/java/y2016/d09/Part2.java
@@ -0,0 +1,58 @@
+package y2016.d09;
+
+import common.Files;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Part2 {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        ArrayList<String> in = Files.readByLines("src/main/java/y2016/d09/in.txt");
+
+        String oldData = in.get(0);
+        String newData = "";
+        while (true) {
+            newData = unpack(oldData);
+
+            System.out.println(newData.length());
+            if (oldData.length() == newData.length()) {
+                break;
+            }
+
+            oldData = newData;
+        }
+    }
+
+    private static String unpack(String packed) {
+        StringBuilder unpacked = new StringBuilder();
+        Pattern marker = Pattern.compile("^\\((\\d+)x(\\d+)\\)");
+
+        for (int i = 0; i < packed.length(); i++) {
+            if (packed.charAt(i) != '(') {
+                unpacked.append(packed.charAt(i));
+                continue;
+            }
+            if (packed.substring(i).matches("^\\((\\d+)x(\\d+)\\).*")) {
+                Matcher m = marker.matcher(packed.substring(i));
+                m.find();
+                int length = Integer.parseInt(m.group(1));
+                int times  = Integer.parseInt(m.group(2));
+                i += 3 + m.group(1).length() + m.group(2).length();
+
+                String clone = packed.substring(i, i+length);
+                for (int c = 0; c < times; c++) {
+                    unpacked.append(clone);
+                }
+
+                i += length-1;
+            }
+        }
+
+        return unpacked.toString();
+    }
+
+}
