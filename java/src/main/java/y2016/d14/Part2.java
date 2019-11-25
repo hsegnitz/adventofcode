5a111bae3926af495287c685a5e26e8a61dc70aa
Holger Segnitz <holger@segnitz.net>
2019-11-25 23:04:19 +0100

    2016.14.2 -- *whew*

--- java/src/main/java/y2016/d14/Part2.java
+++ java/src/main/java/y2016/d14/Part2.java
@@ -4,35 +4,35 @@ import org.apache.commons.codec.binary.Hex;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Part1 {
+public class Part2 {
 
     private static MessageDigest md5;
     private static Pattern pattern1 = Pattern.compile("([a-f0-9])(\\1{2})");
+    // private static String salt = "abc";
+    private static String salt = "cuanljph";
+
+    private static HashMap<Integer, String> hashMap = new HashMap<Integer, String>();
 
     public static void main(String[] args) throws NoSuchAlgorithmException {
         md5 = MessageDigest.getInstance("MD5");
 
-        //String salt = "abc";
-        String salt = "cuanljph";
-
-        int count = 0;
-
-
         HashSet<String> keys = new HashSet<String>();
 
+        int count = 0;
         while (keys.size() < 64) {
-            String hash = getMd5Hex(salt + count);
+            String hash = hashOrDie(count);
             String result3 = getCharOfSequenceOfThree(hash);
 
             if (null != result3) {
 
                 String hash2;
                 for (int i = count + 1; i < count + 1000; i++) {
-                    hash2 = getMd5Hex(salt + i);
+                    hash2 = hashOrDie(i);
                     if (hasSequenceOfFive(hash2, result3)) {
                         System.out.print("three (" + result3 + "):" + hash + " @ " + count);
                         System.out.print("   five (" + result3 + "):" + hash2 + " @ " + i + "   |   ");
@@ -46,6 +46,19 @@ public class Part1 {
         }
 
         System.out.println(count-1);
+    }
+
+    private static String hashOrDie(int count) {
+        if (hashMap.containsKey(count)) {
+            return hashMap.get(count);
+        }
+
+        String hash = getMd5Hex(salt + count);
+        for (int i = 0; i < 2016; i++ ) {
+            hash = getMd5Hex(hash);
+        }
+        hashMap.put(count, hash);
+        return hash;
     }
 
     private static String getCharOfSequenceOfThree(String hash) {
