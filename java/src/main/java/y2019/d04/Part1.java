4432d7df5e18efcca208b318657ee0742272fb98
Holger Segnitz <holger@segnitz.net>
2019-12-04 07:46:40 +0100

    2019.04.2

--- java/src/main/java/y2019/d04/Part1.java
+++ java/src/main/java/y2019/d04/Part1.java
@@ -3,32 +3,48 @@ package y2019.d04;
 public class Part1 {
 
     public static void main(String[] args) {
-        int count = 0;
+        int count1 = 0;
         for (int i = 367479; i <= 893698; i++) {
-            if (isValid(i)) {
-                ++count;
+            if (isValid1(i)) {
+                ++count1;
             }
         }
-        System.out.println(count);
+        System.out.println(count1);
+
+        int count2 = 0;
+        for (int i = 367479; i <= 893698; i++) {
+            if (isValid2(i)) {
+                ++count2;
+            }
+        }
+        System.out.println(count2);
     }
 
-    public static boolean isValid(Integer input) {
+    public static boolean isValid1(Integer input) {
         String in = input.toString();
         if (! in.matches("^\\d*(\\d)(\\1)\\d*$")) {
             return false;
         }
 
-        if (
-                Integer.parseInt(in.substring(0, 1)) > Integer.parseInt(in.substring(1, 2))
-                || Integer.parseInt(in.substring(1, 2)) > Integer.parseInt(in.substring(2, 3))
-                || Integer.parseInt(in.substring(2, 3)) > Integer.parseInt(in.substring(3, 4))
-                || Integer.parseInt(in.substring(3, 4)) > Integer.parseInt(in.substring(4, 5))
-                || Integer.parseInt(in.substring(4, 5)) > Integer.parseInt(in.substring(5))
-        ){
-            return false;
-        }
+        return isAscending(in);
+    }
 
-        return true;
+    public static boolean isValid2(Integer input) {
+        String in = input.toString();
+        if (!isAscending(in)) return false;
+
+        String in2 = in.replaceAll("(\\d)\\1{2,}", "x");
+ //       System.out.println(in2);
+
+        return in2.matches("^.*(\\d)(\\1).*$");
+    }
+
+    private static boolean isAscending(String in) {
+        return Integer.parseInt(in.substring(0, 1)) <= Integer.parseInt(in.substring(1, 2))
+                && Integer.parseInt(in.substring(1, 2)) <= Integer.parseInt(in.substring(2, 3))
+                && Integer.parseInt(in.substring(2, 3)) <= Integer.parseInt(in.substring(3, 4))
+                && Integer.parseInt(in.substring(3, 4)) <= Integer.parseInt(in.substring(4, 5))
+                && Integer.parseInt(in.substring(4, 5)) <= Integer.parseInt(in.substring(5));
     }
 
 }
