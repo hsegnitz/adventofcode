9920ce8eadbe8f3804ec48179b2cc1a5d58bf9e0
Holger Segnitz <holger@segnitz.net>
2019-12-08 11:00:53 +0100

    2019.08.1

---
+++ java/src/main/java/y2019/d08/Part1.java
@@ -0,0 +1,37 @@
+package y2019.d08;
+
+import common.Files;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class Part1 {
+
+    private static final int width  = 25;
+    private static final int height =  6;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<String> input = Files.readByLines("src/main/java/y2019/d08/in.txt");
+        String theInput = input.get(0);
+
+        int step = width * height;
+        int numberOfZeros = Integer.MAX_VALUE;
+        String candidate = "";
+        for (int i = 0; i < theInput.length(); i += step) {
+            String substr = theInput.substring(i, i+step);
+            int zeroCount = countChars(substr, "0");
+            if (zeroCount < numberOfZeros) {
+                numberOfZeros = zeroCount;
+                candidate = substr;
+            }
+        }
+
+        System.out.println(countChars(candidate, "1") * countChars(candidate, "2"));
+
+    }
+
+    private static int countChars(String input, String character) {
+        return input.length() - input.replace(character, "").length();
+    }
+
+}
