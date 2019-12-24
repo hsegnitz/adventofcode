393dc3e035b5bb0a9132ca4fa2db770b996aab30
Holger Segnitz <holger@segnitz.net>
2019-12-24 12:49:30 +0100

    ADD: correct GoL implementation for demo content

---
+++ java/src/main/java/y2019/d24/Part1.java
@@ -0,0 +1,99 @@
+package y2019.d24;
+
+import java.util.HashSet;
+
+public class Part1 {
+
+    /** demo content */
+    private static boolean[][] map = {
+            { false, false, false, false, true  },
+            { true,  false, false, true,  false },
+            { true,  false, false, true,  true  },
+            { false, false, true,  false, false },
+            { true,  false, false, false, false }
+    };
+    /*     */
+
+    /** real content * /
+    private static boolean[][] map = {
+            { false, false, false, false, true  },
+            { true,  false, false, true,  false },
+            { true,  false, false, true,  true  },
+            { false, false, true,  false, false },
+            { true,  false, false, false, false }
+    };
+    /*     */
+
+    private static HashSet<String> seen = new HashSet<>();
+
+    public static void main(String[] args) {
+
+        String mapString = mapToString(map);
+        seen.add(mapString);
+
+        System.out.println(mapString);
+
+        // System.out.println(mapToString(map));
+        while (true) {
+            boolean[][] newMap = new boolean[5][5];
+            for (int y = 0; y < 5; y++) {
+                for (int x = 0; x < 5; x++) {
+                    newMap[y][x] = willLive(x, y);
+                }
+            }
+
+            mapString = mapToString(newMap);
+
+            if (!seen.add(mapString)) {
+                break;
+            }
+            //System.out.println(mapString);
+            map = newMap;
+        }
+
+        System.out.println(mapString);
+    }
+
+    private static boolean willLive(int x, int y) {
+        int neighbors = countNeighbors(x, y);
+
+        if (map[y][x]) {
+            if (neighbors == 1) {
+                return true;
+            }
+            return false;
+        }
+
+        if (!map[y][x]) {
+            if (neighbors == 1 || neighbors == 2) {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    private static int countNeighbors(int x, int y) {
+        int neighbors = 0;
+
+        if (x > 0 && map[y][x-1]) neighbors++;
+        if (x < 4 && map[y][x+1]) neighbors++;
+        if (y > 0 && map[y-1][x]) neighbors++;
+        if (y < 4 && map[y+1][x]) neighbors++;
+
+        return neighbors;
+    }
+
+    private static String mapToString(boolean[][] map) {
+        StringBuilder out = new StringBuilder();
+        for (boolean[] line: map) {
+            for (boolean field: line) {
+                out.append(field ? "#" : ".");
+            }
+            out.append("\n");
+        }
+        return out.toString();
+    }
+
+
+}
