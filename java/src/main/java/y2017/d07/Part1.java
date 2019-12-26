c74c060f3e4a55d30c8749560544d7dfeac9f52d
Holger Segnitz <holger@segnitz.net>
2019-12-26 18:54:15 +0100

    ADD: demo, input and short instruction for offline solving

---
+++ java/src/main/java/y2017/d07/Part1.java
@@ -0,0 +1,10 @@
+package y2017.d07;
+
+public class Part1 {
+
+    // build a tree, find out the root element
+    // actually, lazy method: just find the element that is not included in one of the lists but bears other programs
+
+    // "tknk" for the example
+
+}
