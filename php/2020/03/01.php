c36d408439d398fa4229351a727b62c5ae274d04
Holger Segnitz <holger@segnitz.net>
2020-12-03 07:00:13 +0100

    2020.03.1.demo

---
+++ php/2020/03/01.php
@@ -0,0 +1,16 @@
+<?php
+
+$map = file('./demo.txt');
+
+$right = 3;
+$rowLength = strlen(trim($map[0]));
+
+$count = 0;
+for ($row = 1, $rowMax = count($map); $row < $rowMax; $row++) {
+    $pos = ($right * $row) % $rowLength;
+    if ($map[$row][$pos] === '#') {
+        $count++;
+    }
+}
+
+echo $count, "\n";
