b01b7f523d464e39bc32016ac74ccb17f505c2bb
Holger Segnitz <holger@segnitz.net>
2020-12-04 07:12:30 +0100

    2020.04.2.demo

--- php/2020/04/01.php
+++ php/2020/04/01.php
@@ -2,16 +2,21 @@
 
 require_once './passport.php';
 
-$rawInput = file_get_contents('./demo.txt');
+//$rawInput = file_get_contents('./demo.txt');
+$rawInput = file_get_contents('./demo2.txt');
 //$rawInput = file_get_contents('./in.txt');
 
 $rawPassports = explode("\n\n", $rawInput);
-$countValid = 0;
+$countValid1 = $countValid2 = 0;
 foreach ($rawPassports as $rawPassport) {
     $passport = new passport($rawPassport);
-    if ($passport->isValidDay1()) {
-        ++$countValid;
+    if ($passport->isValid1()) {
+        ++$countValid1;
+        if ($passport->isValid2()) {
+            ++$countValid2;
+        }
     }
 }
 
-echo $countValid, "\n";
+echo "valid 1: ", $countValid1, "\n";
+echo "valid 2: ", $countValid2, "\n";
--- php/2020/04/passport.php
+++ php/2020/04/passport.php
@@ -30,7 +30,7 @@ class passport
         }
     }
 
-    public function isValidDay1(): bool
+    public function isValid1(): bool
     {
         if (null === $this->birthYear) {
             return false;
@@ -57,6 +57,39 @@ class passport
         }
 
         if (null === $this->passportId) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function isValid2(): bool
+    {
+        if (!preg_match('/^\d{4}$/', $this->birthYear) || 1920 > $this->birthYear || 2002 < $this->birthYear) {
+            return false;
+        }
+
+        if (!preg_match('/^\d{4}$/', $this->issueYear) || 2010 > $this->issueYear || 2020 < $this->issueYear) {
+            return false;
+        }
+
+        if (!preg_match('/^\d{4}$/', $this->expirationYear) || 2020 > $this->expirationYear || 2030 < $this->expirationYear) {
+            return false;
+        }
+
+        if (!preg_match('/(\d+)(cm|in)/', $this->height, $heightOut) || ($heightOut[2] === 'cm' && (150 > $heightOut[1] || 193 < $heightOut[1])) || ($heightOut[2] === 'in' && (59 > $heightOut[1] || 76 < $heightOut[1]))) {
+            return false;
+        }
+
+        if (!preg_match('/^#[0-9a-f]{6}$/', $this->hairColor)) {
+            return false;
+        }
+
+        if (!in_array($this->eyeColor, ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'])) {
+            return false;
+        }
+
+        if (!preg_match('/^[\d]{9}$/', $this->passportId)) {
             return false;
         }
 
