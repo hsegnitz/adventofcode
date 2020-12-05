07a0f01147d8307eec3160223daf1c4ae318ac54
Holger Segnitz <holger@segnitz.net>
2020-12-05 07:03:49 +0100

    2020.05.1 -- refactor for reusability

--- php/2020/05/1.php
+++ php/2020/05/1.php
@@ -8,28 +8,32 @@ class BoardingPass
 
     public function __construct(string $bsp)
     {
-        $lower = 0;
-        $upper = 127;
+        $this->row = $this->binarySearch(substr($bsp, 0, 7), 'F');
+    }
 
-        for ($i = 0; $i < 6; $i++) {
+    public function getRow(): int
+    {
+        return $this->row;
+    }
+
+    private function binarySearch(string $bsp, string $charForLower): int
+    {
+        $lower = 0;
+        $upper = (2 ** strlen($bsp)) -1;
+
+        for ($i = 0; $i < strlen($bsp)-1; $i++) {
             $change = ($upper - $lower + 1) / 2;
-            if ($bsp[$i] === 'F') {
+            if ($bsp[$i] === $charForLower) {
                 $upper -= $change;
             } else {
                 $lower += $change;
             }
         }
 
-        if ($bsp[6] === 'F') {
-            $this->row = $lower;
-        } else {
-            $this->row = $upper;
+        if ($bsp[strlen($bsp)-1] === $charForLower) {
+            return $lower;
         }
-    }
-
-    public function getRow(): int
-    {
-        return $this->row;
+        return $upper;
     }
 
 }
