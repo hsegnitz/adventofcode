d0e44c7d234c722dfb25115d76852a86ad15ce72
Holger Segnitz <holger@segnitz.net>
2020-12-05 15:33:54 +0100

    2016.11.1 -- looping math fails

--- php/2016/11/1.php
+++ php/2016/11/1.php
@@ -6,31 +6,51 @@
 
 // manual reading
 // actual
-//$itemsDistribution = [
-//    0 => 2,
-//    1 => 4,
-//    2 => 4,
-//    3 => 0,
-//];
-
-// demo
 $itemsDistribution = [
     0 => 2,
-    1 => 1,
-    2 => 1,
+    1 => 4,
+    2 => 4,
     3 => 0,
 ];
 
-function minMoves($itemsDistribution): int
-{
-    $itemCount = array_sum($itemsDistribution);
+// demo
+//$itemsDistribution = [
+//    0 => 2,
+//    1 => 1,
+//    2 => 1,
+//    3 => 0,
+//];
 
+function minMoves($floors): int
+{
+    $itemCount = array_sum($floors);
     $moves = 0;
     $floor = 0;
-    while ($itemsDistribution[3] !== $itemCount) {
-        $moves += (2 * $itemsDistribution[$floor]) - 3;
-        $itemsDistribution[$floor + 1] += $itemsDistribution[$floor];
-        $floor++;
+    $onElevator = min($floors[0], 2);
+
+    while ($floors[3]+1 !== $itemCount) {
+        while ($onElevator < 2 && $floor > 0) {
+            $floor--;
+            $pickedUpItems = min($floors[$floor], 2 - $onElevator);
+            if ($pickedUpItems > 0) {
+                $onElevator += $pickedUpItems;
+                $floors[$floor] -= $pickedUpItems;
+            }
+            $moves++;
+        }
+
+        while ($floor < 3) {
+            $floor++;
+            $pickedUpItems = min($floors[$floor], 2 - $onElevator);
+            if ($pickedUpItems > 0) {
+                $onElevator += $pickedUpItems;
+                $floors[$floor] -= $pickedUpItems;
+            }
+            $moves++;
+        }
+
+        $floors[3] += 1;
+        $onElevator--;
     }
 
     return $moves;
