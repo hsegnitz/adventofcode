a0c63df39d5b67259c731e66bb136c483c209fc2
Holger Segnitz <holger@segnitz.net>
2020-12-08 06:09:48 +0100

    2020.08.1
    113

---
+++ php/2020/08/1.php
@@ -0,0 +1,32 @@
+<?php
+
+//$input = file('demo.txt');
+$input = file('in.txt');
+
+$instructions = [];
+foreach ($input as $instruction) {
+    $split = explode(" ", $instruction);
+    $instructions[] = [
+        'cmd' => $split[0],
+        'val' => (int)$split[1],
+    ];
+}
+
+$accumulator = 0;
+$pointer = 0;
+$visited = [];
+while (!isset($visited[$pointer])) {
+    $visited[$pointer] = true;
+    switch ($instructions[$pointer]['cmd']) {
+        case "acc":
+            $accumulator += $instructions[$pointer]['val'];
+        case "nop":
+            $pointer++;
+            break;
+        case "jmp":
+            $pointer += $instructions[$pointer]['val'];
+            break;
+    }
+}
+
+echo $accumulator;
