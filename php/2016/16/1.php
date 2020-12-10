a0c3299ff318dde1def65468300d734308918f06
Holger Segnitz <holger@segnitz.net>
2020-12-10 22:40:01 +0100

    2016.16.1
    1280

--- php/2016/16/1.php
+++ php/2016/16/1.php
@@ -12,4 +12,31 @@ while (strlen($pattern) < $size) {
     $pattern = dragon($pattern);
 }
 
-echo $pattern;
+$pattern = substr($pattern, 0, $size);
+#echo $pattern;
+
+function checksum($in): string
+{
+    $ret = '';
+    foreach (str_split($in, 2) as $item) {
+        switch ($item) {
+            case "00":
+            case "11":
+                $ret .= '1';
+                break;
+            case "01":
+            case "10":
+                $ret .= '0';
+                break;
+        }
+    }
+    return $ret;
+}
+
+$checksum = $pattern;
+while (0 === strlen($checksum) % 2) {
+    $checksum = checksum($checksum);
+}
+
+echo $checksum, "\n";
+
