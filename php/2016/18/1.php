a0ceab5ed4d105be6898479c95ea07443cb9902a
Holger Segnitz <holger@segnitz.net>
2020-12-12 19:53:53 +0100

    2016.18.1
    8362
    
    427
    
    59

---
+++ php/2016/18/1.php
@@ -0,0 +1,52 @@
+<?php
+
+$startTime = microtime(true);
+
+// $start = '..^^.';
+// $start = '.^^.^.^^^^';
+$start = '.^.^..^......^^^^^...^^^...^...^....^^.^...^.^^^^....^...^^.^^^...^^^^.^^.^.^^..^.^^^..^^^^^^.^^^..^';  // zeh real one
+
+$field = [
+    str_split($start),
+];
+
+function walk(array &$field) {
+    $previous = $field[count($field)-1];
+    $current = [];
+    for ($i = 0, $iMax = count($previous); $i < $iMax; $i++) {
+        $current[] = ackbar($previous, $i) ? '^' : '.';
+    }
+    $field[] = $current;
+}
+
+// as in "It's a trap!"
+function ackbar(array $previous, int $pos): bool
+{
+    $segment = [
+        $previous[$pos - 1] ?? '.',
+        $previous[$pos],
+        $previous[$pos + 1] ?? '.',
+    ];
+
+    return ($segment === ['^', '^', '.'] || $segment === ['.', '^', '^'] || $segment === ['^', '.', '.'] || $segment === ['.', '.', '^']);
+}
+
+function serializeField(array $field): string
+{
+    $ret = '';
+    foreach ($field as $row) {
+        $ret .= implode('', $row) . "\n";
+    }
+    return $ret;
+}
+
+while (count($field) < 40) {
+    walk($field);
+}
+
+
+echo $s = serializeField($field), "\n";
+echo count_chars($s, 1)[46], "\n";  // 46 === '.'
+
+
+echo "total time: ", (microtime(true) - $startTime), "\n";
