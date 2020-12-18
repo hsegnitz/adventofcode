a0cef28a254604561ff0753bf8d2aa64db02c396
Holger Segnitz <holger@segnitz.net>
2020-12-18 06:21:48 +0100

    2020.18.1-demo1
    3850

---
+++ php/2020/18/1.php
@@ -0,0 +1,47 @@
+<?php
+
+$startTime = microtime(true);
+
+$input = file(__DIR__ . '/demo.txt');
+
+function solve(string $string): int
+{
+    $string = trim($string);
+    /**
+    while (false !== strpos($string, '(')) {
+        // recursively replace innermost parentheses until non left
+    }*/
+
+    // from here on no parentheses left!
+
+    $tokens = explode(' ', $string);
+    while (count($tokens) > 1) {
+        $subj1 = (int)array_shift($tokens);
+        $op    = array_shift($tokens);
+        $subj2 = (int)array_shift($tokens);
+        if ($op === '+') {
+            array_unshift($tokens, $subj1 + $subj2);
+        } elseif ($op === '*') {
+            array_unshift($tokens, $subj1 * $subj2);
+        } else {
+            throw new RuntimeException('WTF Dude!?');
+        }
+    }
+
+    return array_shift($tokens);
+}
+
+
+echo solve($input[0]), "\n";
+die();
+
+
+$solutions = [];
+foreach ($input as $row) {
+
+}
+
+
+
+echo "\ntotal time: ", (microtime(true) - $startTime), "\n";
+
