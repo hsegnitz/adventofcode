a0c6649c71f916bd2ee5739481ef8b739fdbea88
Holger Segnitz <holger@segnitz.net>
2020-12-26 15:07:09 +0100

    2017.09.x unittest cleanup
    2752

--- php/2017/09/GarbageGourmetTest.php
+++ php/2017/09/GarbageGourmetTest.php
@@ -8,17 +8,28 @@ class GarbageGourmetTest extends TestCase
 {
 
     /**
-     * @dataProvider demo
+     * @dataProvider demoGroupScores
      * @param string $input
      * @param int $expected
      */
-    public function testTop(string $input, int $expected): void
+    public function testScore(string $input, int $expected): void
     {
         $garbageGourmet = new GarbageGourmet($input);
-        $this->assertSame($expected, $garbageGourmet->score($input));
+        $this->assertSame($expected, $garbageGourmet->score());
     }
 
-    public function demo(): array
+    /**
+     * @dataProvider demoGarbageScores
+     * @param string $input
+     * @param int $expected
+     */
+    public function testGarbageSize(string $input, int $expected): void
+    {
+        $garbageGourmet = new GarbageGourmet($input);
+        $this->assertSame($expected, $garbageGourmet->getGarbageSize());
+    }
+
+    public function demoGroupScores(): array
     {
         return [
             0 => ['{}', 1],
@@ -32,6 +43,16 @@ class GarbageGourmetTest extends TestCase
         ];
     }
 
-
+    public function demoGarbageScores(): array
+    {
+        return [
+            0 => ['<>', 0],
+            1 => ['<random characters>', 17],
+            2 => ['<<<<>', 3],
+            3 => ['<{!>}>', 2],
+            4 => ['<!!>', 0],
+            5 => ['<!!!>>', 0],
+            6 => ['<{o"i!a,<{i<a>', 10],
+        ];
+    }
 }
-
