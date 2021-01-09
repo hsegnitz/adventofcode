a0c6a65d85d8ec3503b24121f3553395a8f29bf3
Holger Segnitz <holger@segnitz.net>
2021-01-09 20:57:11 +0100

    2017.12.2 - demo
    533

--- php/2017/12/1.php
+++ php/2017/12/1.php
@@ -3,7 +3,7 @@
 $startTime = microtime(true);
 
 $pipes = [];
-foreach (file(__DIR__ . '/in.txt') as $num => $rawLine) {
+foreach (file(__DIR__ . '/demo.txt') as $num => $rawLine) {
     if (preg_match('/(\d+) <-> ([\d, ]+)/', $rawLine, $out)) {
         $pipes[$out[1]] = explode(', ', $out[2]);
     } else {
@@ -13,20 +13,38 @@ foreach (file(__DIR__ . '/in.txt') as $num => $rawLine) {
 
 #print_r($pipes);
 
-$targetPipes = [
-    0 => true,
-];
-$oldTargetPipes = [];
-while ($targetPipes !== $oldTargetPipes) {
-    $oldTargetPipes = $targetPipes;
-    foreach ($oldTargetPipes as $id => $blubb) {
-        foreach ($pipes[$id] as $target) {
-            $targetPipes[$target] = true;
+function findClosedCircleOf(int $targetId, array $pipes): array
+{
+    $targetPipes = [
+        $targetId => true,
+    ];
+    $oldTargetPipes = [];
+    while ($targetPipes !== $oldTargetPipes) {
+        $oldTargetPipes = $targetPipes;
+        foreach (array_keys($oldTargetPipes) as $id) {
+            foreach ($pipes[$id] as $target) {
+                $targetPipes[$target] = true;
+            }
         }
     }
+
+    $targetPipes = array_keys($targetPipes);
+    sort($targetPipes);
+    return $targetPipes;
 }
 
-echo implode(', ', array_keys($targetPipes)), "\n";
+$targetPipes = findClosedCircleOf(0, $pipes);
+echo implode(', ', $targetPipes), "\n";
 echo count($targetPipes), "\n";
+
+$allClosedCircles = [];
+foreach (array_keys($pipes) as $pipeId) {
+    $allClosedCircles[] = findClosedCircleOf($pipeId, $pipes);
+}
+
+$unique = array_unique($allClosedCircles, SORT_REGULAR);
+
+echo count($unique), "\n";
+
 echo "total time: ", (microtime(true) - $startTime), "\n";
 
