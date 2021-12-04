a0c8d47287d2e545f98768a1458038974f8158d0
Holger Segnitz <holger@segnitz.net>
2021-12-04 07:11:37 +0100

    2021.04
    14158

---
+++ php/2021/04/01.php
@@ -0,0 +1,93 @@
+<?php
+
+$startTime = microtime(true);
+
+#$input = file('./example.txt');
+$input = file('./in.txt');
+
+$first = array_shift($input);
+$numbers = explode(",", trim($first));
+
+$rawBoards = array_chunk($input, 6);
+
+$boards = [];
+foreach ($rawBoards as $rawBoard) {
+    array_shift($rawBoard);
+    $boards[] = new board($rawBoard);
+}
+
+foreach ($numbers as $number) {
+    $numAsInt = (int)$number;
+    foreach ($boards as $board) {
+        $board->registerNumber($number);
+        if  ($board->isWinner()) {
+            break 2;
+        }
+    }
+}
+
+$result = $number * $board->getSumUnmarked();
+
+echo $result, "\ntotal time: ", (microtime(true) - $startTime), "\n";
+
+
+
+class board {
+
+    private $board = [];
+
+    public function __construct(array $rawBoard)
+    {
+        foreach ($rawBoard as $row) {
+            $this->board[] = preg_split('/\s+/', trim($row));
+        }
+    }
+
+    public function registerNumber(int $number): void
+    {
+        foreach ($this->board as $rowNum => $row) {
+            foreach ($row as $colNum => $cell) {
+                if ($cell == $number) {
+                    $this->board[$rowNum][$colNum] = -1;
+                    return;
+                }
+            }
+        }
+    }
+
+    public function isWinner(): bool
+    {
+        $target = array_fill(0, 5, -1);
+        foreach ($this->board as $rowNum => $row) {
+            if ($row == $target) {
+                return true;
+            }
+            if (array_column($this->board, $rowNum) == $target) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function __toString(): string
+    {
+        $out = [];
+        foreach ($this->board as $row) {
+            $out[] = implode("  ", $row);
+        }
+        return implode("\n", $out);
+    }
+
+    public function getSumUnmarked(): int
+    {
+        $sum = 0;
+        foreach ($this->board as $row) {
+            foreach ($row as $cell) {
+                if ($cell > -1) {
+                    $sum += $cell;
+                }
+            }
+        }
+        return $sum;
+    }
+}
---
+++ php/2021/04/02.php
@@ -0,0 +1,106 @@
+<?php
+
+$startTime = microtime(true);
+
+#$input = file('./example.txt');
+$input = file('./in.txt');
+
+$first = array_shift($input);
+$numbers = explode(",", trim($first));
+
+$rawBoards = array_chunk($input, 6);
+
+$boards = [];
+foreach ($rawBoards as $rawBoard) {
+    array_shift($rawBoard);
+    $boards[] = new board($rawBoard);
+}
+
+$boardsWinAfter = [];
+foreach ($boards as $board) {
+    foreach ($numbers as $numNum => $num) {
+        $board->registerNumber((int)$num);
+        if ($board->isWinner()) {
+            $boardsWinAfter[$numNum] = $board;
+            break;
+        }
+    }
+}
+
+ksort($boardsWinAfter);
+
+/** @var board $last */
+$last = array_pop($boardsWinAfter);
+
+$result = $last->getScore();
+
+echo $result, "\ntotal time: ", (microtime(true) - $startTime), "\n";
+
+
+
+class board {
+
+    private array $board = [];
+    private int $lastNumber;
+
+    public function __construct(array $rawBoard)
+    {
+        foreach ($rawBoard as $row) {
+            $this->board[] = preg_split('/\s+/', trim($row));
+        }
+    }
+
+    public function registerNumber(int $number): void
+    {
+        $this->lastNumber = $number;
+        foreach ($this->board as $rowNum => $row) {
+            foreach ($row as $colNum => $cell) {
+                if ($cell == $number) {
+                    $this->board[$rowNum][$colNum] = -1;
+                    return;
+                }
+            }
+        }
+    }
+
+    public function isWinner(): bool
+    {
+        $target = array_fill(0, 5, -1);
+        foreach ($this->board as $rowNum => $row) {
+            if ($row == $target) {
+                return true;
+            }
+            if (array_column($this->board, $rowNum) == $target) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function __toString(): string
+    {
+        $out = [];
+        foreach ($this->board as $row) {
+            $out[] = implode("  ", $row);
+        }
+        return implode("\n", $out);
+    }
+
+    public function getSumUnmarked(): int
+    {
+        $sum = 0;
+        foreach ($this->board as $row) {
+            foreach ($row as $cell) {
+                if ($cell > -1) {
+                    $sum += $cell;
+                }
+            }
+        }
+        return $sum;
+    }
+
+    public function getScore(): int
+    {
+        return $this->lastNumber * $this->getSumUnmarked();
+    }
+}
