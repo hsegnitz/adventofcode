a0c1bd69ab2b9508c56ea9be8ee37fb56624b7bc
Holger Segnitz <holger@segnitz.net>
2021-12-15 21:25:59 +0100

    2021.15.1
    982

--- php/2021/15/01.php
+++ php/2021/15/01.php
@@ -2,33 +2,8 @@
 
 $startTime = microtime(true);
 
-$input = file_get_contents('./example.txt');
-#$input = file_get_contents('./in.txt');
-
-$rows = explode("\n", $input);
-
-$map = [];
-foreach ($rows as $row) {
-    $map[] = str_split(trim($row));
-}
-
-function getNeighbours(int $x, int $y, $map): array
-{
-    $neighbours = [];
-    if (isset($map[$y-1][$x])) {
-        $neighbours[] = $map[$y-1][$x];
-    }
-    if (isset($map[$y][$x-1])) {
-        $neighbours[] = $map[$y][$x-1];
-    }
-    if (isset($map[$y][$x+1])) {
-        $neighbours[] = $map[$y][$x+1];
-    }
-    if (isset($map[$y+1][$x])) {
-        $neighbours[] = $map[$y+1][$x];
-    }
-    return $neighbours;
-}
+#$input = file_get_contents('./example.txt');
+$input = file_get_contents('./in.txt');
 
 class AdventOfHeap extends SplMinHeap
 {
@@ -38,9 +13,77 @@ class AdventOfHeap extends SplMinHeap
     }
 }
 
-$visited = [];
-$heap = new AdventOfHeap();
-$heap->insert(['x' => 0, 'y' => 0, 'distance' => 0]);
+
+class Solver {
+
+    private array $map = [];
+    private array $visited = [];
+    private AdventOfHeap $heap;
+    private string $finalCoord;
+
+    public function __construct(string $input)
+    {
+        $rows = explode("\n", $input);
+        foreach ($rows as $row) {
+            $this->map[] = str_split(trim($row));
+        }
+
+        $this->finalCoord = count($this->map[0])-1 . "," . count($this->map)-1;
+
+        $this->heap = new AdventOfHeap();
+        $this->heap->insert(['x' => 0, 'y' => 0, 'distance' => 0]);
+    }
+
+    private function getNeighbours(int $x, int $y): array
+    {
+        $neighbours = [];
+        if (isset($this->map[$y-1][$x])) {
+            $neighbours[] = ['x' => $x,   'y' => $y-1, 'distance' => $this->map[$y-1][$x]];
+        }
+        if (isset($this->map[$y][$x-1])) {
+            $neighbours[] = ['x' => $x-1, 'y' => $y,   'distance' => $this->map[$y][$x-1]];
+        }
+        if (isset($this->map[$y][$x+1])) {
+            $neighbours[] = ['x' => $x+1, 'y' => $y,   'distance' => $this->map[$y][$x+1]];
+        }
+        if (isset($this->map[$y+1][$x])) {
+            $neighbours[] = ['x' => $x,   'y' => $y+1, 'distance' => $this->map[$y+1][$x]];
+        }
+        return $neighbours;
+    }
+
+    public function walk(): void
+    {
+        while (true) {
+            $current = $this->heap->extract();
+
+            foreach ($this->getNeighbours($current['x'], $current['y']) as $next) {
+                $coord = $next['x'] . "," .  $next['y'];
+                if (isset($this->visited[$coord])) {
+                    continue;
+                }
+                $next['distance'] += $current['distance'];
+                $this->heap->insert($next);
+                $this->visited[$coord] = $next['distance'];
+
+                if ($coord === $this->finalCoord) {
+                    return;
+                }
+            }
+        }
+    }
+
+    public function getDistanceOfFinalTile(): int
+    {
+        return $this->visited[$this->finalCoord];
+    }
+}
+
+$solver = new Solver($input);
+
+$solver->walk();
+
+print_r($solver->getDistanceOfFinalTile());
 
 echo "\ntotal time: ", (microtime(true) - $startTime), "\n";
 
