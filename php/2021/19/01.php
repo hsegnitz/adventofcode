a0c2f8bdd80173e4e868ade20c1f00da74f9db6e
Holger Segnitz <holger@segnitz.net>
2021-12-19 21:52:27 +0100

    2021.19.1
    3827

--- php/2021/19/01.php
+++ php/2021/19/01.php
@@ -46,58 +46,63 @@ class Transformer
     public function __construct()
     {
         $this->functions['XYZ'] = static function (Coord $coords):Coord { return $coords; };
+        $this->functions['Xyz'] = static function (Coord $coords):Coord { return new Coord($coords->x,    $coords->y*-1, $coords->z*-1); };
+        $this->functions['XZy'] = static function (Coord $coords):Coord { return new Coord($coords->x,    $coords->z,    $coords->y*-1); };
+        $this->functions['XzY'] = static function (Coord $coords):Coord { return new Coord($coords->x,    $coords->z*-1, $coords->y   ); };
+        $this->functions['YZX'] = static function (Coord $coords):Coord { return new Coord($coords->y,    $coords->z,    $coords->x   ); };
+        $this->functions['Yzx'] = static function (Coord $coords):Coord { return new Coord($coords->y,    $coords->z*-1, $coords->x*-1); };
+        $this->functions['YXz'] = static function (Coord $coords):Coord { return new Coord($coords->y,    $coords->x,    $coords->z*-1); };
+        $this->functions['YxZ'] = static function (Coord $coords):Coord { return new Coord($coords->y,    $coords->x*-1, $coords->z   ); };
+        $this->functions['ZXY'] = static function (Coord $coords):Coord { return new Coord($coords->z,    $coords->x,    $coords->y   ); };
+        $this->functions['Zxy'] = static function (Coord $coords):Coord { return new Coord($coords->z,    $coords->x*-1, $coords->y*-1); };
+        $this->functions['ZYx'] = static function (Coord $coords):Coord { return new Coord($coords->z,    $coords->y,    $coords->x*-1); };
+        $this->functions['ZyX'] = static function (Coord $coords):Coord { return new Coord($coords->z,    $coords->y*-1, $coords->x   ); };
+        $this->functions['xyZ'] = static function (Coord $coords):Coord { return new Coord($coords->x*-1, $coords->y*-1, $coords->z   ); };
+        $this->functions['xYz'] = static function (Coord $coords):Coord { return new Coord($coords->x*-1, $coords->y,    $coords->z*-1); };
+        $this->functions['xZY'] = static function (Coord $coords):Coord { return new Coord($coords->x*-1, $coords->z,    $coords->y   ); };
+        $this->functions['xzy'] = static function (Coord $coords):Coord { return new Coord($coords->x*-1, $coords->z*-1, $coords->y*-1); };
+        $this->functions['yzX'] = static function (Coord $coords):Coord { return new Coord($coords->y*-1, $coords->z*-1, $coords->x   ); };
+        $this->functions['yZx'] = static function (Coord $coords):Coord { return new Coord($coords->y*-1, $coords->z,    $coords->x*-1); };
+        $this->functions['yXZ'] = static function (Coord $coords):Coord { return new Coord($coords->y*-1, $coords->x,    $coords->z   ); };
+        $this->functions['yxz'] = static function (Coord $coords):Coord { return new Coord($coords->y*-1, $coords->x*-1, $coords->z*-1); };
+        $this->functions['zxY'] = static function (Coord $coords):Coord { return new Coord($coords->z*-1, $coords->x*-1, $coords->y   ); };
+        $this->functions['zXy'] = static function (Coord $coords):Coord { return new Coord($coords->z*-1, $coords->x,    $coords->y*-1); };
+        $this->functions['zYX'] = static function (Coord $coords):Coord { return new Coord($coords->z*-1, $coords->y,    $coords->x   ); };
+        $this->functions['zyx'] = static function (Coord $coords):Coord { return new Coord($coords->z*-1, $coords->y*-1, $coords->x*-1); };
+
+
+/*
+        Well, these are apparently neither rotations nor flips ;)
         $this->functions['XYz'] = static function (Coord $coords):Coord { return new Coord($coords->x,    $coords->y,    $coords->z*-1); };
         $this->functions['XyZ'] = static function (Coord $coords):Coord { return new Coord($coords->x,    $coords->y*-1, $coords->z   ); };
-        $this->functions['Xyz'] = static function (Coord $coords):Coord { return new Coord($coords->x,    $coords->y*-1, $coords->z*-1); };
         $this->functions['xYZ'] = static function (Coord $coords):Coord { return new Coord($coords->x*-1, $coords->y,    $coords->z   ); };
-        $this->functions['xYz'] = static function (Coord $coords):Coord { return new Coord($coords->x*-1, $coords->y,    $coords->z*-1); };
-        $this->functions['xyZ'] = static function (Coord $coords):Coord { return new Coord($coords->x*-1, $coords->y*-1, $coords->z   ); };
         $this->functions['xyz'] = static function (Coord $coords):Coord { return new Coord($coords->x*-1, $coords->y*-1, $coords->z*-1); };
 
         $this->functions['XZY'] = static function (Coord $coords):Coord { return new Coord($coords->x,    $coords->z,    $coords->y   ); };
-        $this->functions['XZy'] = static function (Coord $coords):Coord { return new Coord($coords->x,    $coords->z,    $coords->y*-1); };
-        $this->functions['XzY'] = static function (Coord $coords):Coord { return new Coord($coords->x,    $coords->z*-1, $coords->y   ); };
         $this->functions['Xzy'] = static function (Coord $coords):Coord { return new Coord($coords->x,    $coords->z*-1, $coords->y*-1); };
-        $this->functions['xZY'] = static function (Coord $coords):Coord { return new Coord($coords->x*-1, $coords->z,    $coords->y   ); };
         $this->functions['xZy'] = static function (Coord $coords):Coord { return new Coord($coords->x*-1, $coords->z,    $coords->y*-1); };
         $this->functions['xzY'] = static function (Coord $coords):Coord { return new Coord($coords->x*-1, $coords->z*-1, $coords->y   ); };
-        $this->functions['xzy'] = static function (Coord $coords):Coord { return new Coord($coords->x*-1, $coords->z*-1, $coords->y*-1); };
 
         $this->functions['YXZ'] = static function (Coord $coords):Coord { return new Coord($coords->y,    $coords->x,    $coords->z   ); };
-        $this->functions['YXz'] = static function (Coord $coords):Coord { return new Coord($coords->y,    $coords->x,    $coords->z*-1); };
-        $this->functions['YxZ'] = static function (Coord $coords):Coord { return new Coord($coords->y,    $coords->x*-1, $coords->z   ); };
         $this->functions['Yxz'] = static function (Coord $coords):Coord { return new Coord($coords->y,    $coords->x*-1, $coords->z*-1); };
-        $this->functions['yXZ'] = static function (Coord $coords):Coord { return new Coord($coords->y*-1, $coords->x,    $coords->z   ); };
         $this->functions['yXz'] = static function (Coord $coords):Coord { return new Coord($coords->y*-1, $coords->x,    $coords->z*-1); };
         $this->functions['yxZ'] = static function (Coord $coords):Coord { return new Coord($coords->y*-1, $coords->x*-1, $coords->z   ); };
-        $this->functions['yxz'] = static function (Coord $coords):Coord { return new Coord($coords->y*-1, $coords->x*-1, $coords->z*-1); };
 
-        $this->functions['YZX'] = static function (Coord $coords):Coord { return new Coord($coords->y,    $coords->z,    $coords->x   ); };
         $this->functions['YZx'] = static function (Coord $coords):Coord { return new Coord($coords->y,    $coords->z,    $coords->x*-1); };
         $this->functions['YzX'] = static function (Coord $coords):Coord { return new Coord($coords->y,    $coords->z*-1, $coords->x   ); };
-        $this->functions['Yzx'] = static function (Coord $coords):Coord { return new Coord($coords->y,    $coords->z*-1, $coords->x*-1); };
         $this->functions['yZX'] = static function (Coord $coords):Coord { return new Coord($coords->y*-1, $coords->z,    $coords->x   ); };
-        $this->functions['yZx'] = static function (Coord $coords):Coord { return new Coord($coords->y*-1, $coords->z,    $coords->x*-1); };
-        $this->functions['yzX'] = static function (Coord $coords):Coord { return new Coord($coords->y*-1, $coords->z*-1, $coords->x   ); };
         $this->functions['yzx'] = static function (Coord $coords):Coord { return new Coord($coords->y*-1, $coords->z*-1, $coords->x*-1); };
 
         $this->functions['ZYX'] = static function (Coord $coords):Coord { return new Coord($coords->z,    $coords->y,    $coords->x   ); };
-        $this->functions['ZYx'] = static function (Coord $coords):Coord { return new Coord($coords->z,    $coords->y,    $coords->x*-1); };
-        $this->functions['ZyX'] = static function (Coord $coords):Coord { return new Coord($coords->z,    $coords->y*-1, $coords->x   ); };
         $this->functions['Zyx'] = static function (Coord $coords):Coord { return new Coord($coords->z,    $coords->y*-1, $coords->x*-1); };
-        $this->functions['zYX'] = static function (Coord $coords):Coord { return new Coord($coords->z*-1, $coords->y,    $coords->x   ); };
         $this->functions['zYx'] = static function (Coord $coords):Coord { return new Coord($coords->z*-1, $coords->y,    $coords->x*-1); };
         $this->functions['zyX'] = static function (Coord $coords):Coord { return new Coord($coords->z*-1, $coords->y*-1, $coords->x   ); };
-        $this->functions['zyx'] = static function (Coord $coords):Coord { return new Coord($coords->z*-1, $coords->y*-1, $coords->x*-1); };
 
-        $this->functions['ZXY'] = static function (Coord $coords):Coord { return new Coord($coords->z,    $coords->x,    $coords->y   ); };
         $this->functions['ZXy'] = static function (Coord $coords):Coord { return new Coord($coords->z,    $coords->x,    $coords->y*-1); };
         $this->functions['ZxY'] = static function (Coord $coords):Coord { return new Coord($coords->z,    $coords->x*-1, $coords->y   ); };
-        $this->functions['Zxy'] = static function (Coord $coords):Coord { return new Coord($coords->z,    $coords->x*-1, $coords->y*-1); };
         $this->functions['zXY'] = static function (Coord $coords):Coord { return new Coord($coords->z*-1, $coords->x,    $coords->y   ); };
-        $this->functions['zXy'] = static function (Coord $coords):Coord { return new Coord($coords->z*-1, $coords->x,    $coords->y*-1); };
-        $this->functions['zxY'] = static function (Coord $coords):Coord { return new Coord($coords->z*-1, $coords->x*-1, $coords->y   ); };
         $this->functions['zxy'] = static function (Coord $coords):Coord { return new Coord($coords->z*-1, $coords->x*-1, $coords->y*-1); };
+*/
     }
 
     public function getFunctions(): array
