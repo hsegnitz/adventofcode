a0c211500d8c550127d5e5f7515ca48ab8e82d84
Holger Segnitz <holger@segnitz.net>
2021-12-21 19:16:20 +0100

    2021.21.2
    2441

--- php/2021/21/02.php
+++ php/2021/21/02.php
@@ -3,10 +3,10 @@
 $startTime = microtime(true);
 
 //demo
-$posPlayer1 = 4; $posPlayer2 = 8;
+#$posPlayer0 = 4; $posPlayer1 = 8;
 
 //real
-#$posPlayer1 = 2; $posPlayer2 = 8;
+$posPlayer0 = 2; $posPlayer1 = 8;
 
 // counting possible outcomes of three throws with a 3-sided die   --   1 3 1  is the same like 3 1 1 and the same like 1 2 2
 $universeMultipliers = [
@@ -19,72 +19,57 @@ $universeMultipliers = [
     9 => 1,
 ];
 
-// these indexes are "pos,score" as we can't have arrays as keys in PHP
-// maybe refactor one day into SplObjectStorage?!
-$gamesWithScore1 = ["{$posPlayer1},0" => 1];
-$gamesWithScore2 = ["{$posPlayer2},0" => 1];
 
-$gamesWon1 = 0;
-$gamesWon2 = 0;
+// FORK it maybe cursed recursion solves this
 
-// iteration start
-
-function theForkingGame(array &$myGamesWithScore, array &$theirGamesWithScore, int &$myGamesWon): void
+/**
+ * @return int[]  --  scores for player 1 and 2
+ */
+function theForkingGame(int $posPlayer0, int $scorePlayer0, int $posPlayer1, int $scorePlayer1, int $turn): array
 {
-    $openGames = 0;
-    $newGamesWithScore = [];
-    foreach ($myGamesWithScore as $key => $count) {
-        [$pos, $score] = explode(',', $key);
-        foreach ($GLOBALS['universeMultipliers'] as $positionIncrease => $multiplier) {
-            $tempPos = (((int)$pos - 1 + $positionIncrease) % 10) + 1;
-            $tempScore = (int)$score + $tempPos;
-            $tempCount = $count * $multiplier;
-            if ($tempScore >= 21) {
-                $myGamesWon += $tempCount;
-                continue;
-            }
-            if (!isset($newGamesWithScore["{$tempPos},{$tempScore}"])) {
-                $newGamesWithScore["{$tempPos},{$tempScore}"] = 0;
-            }
-            $newGamesWithScore["{$tempPos},{$tempScore}"] += $tempCount;
-            $openGames += $tempCount;
+    if ($scorePlayer0 >= 21) {
+        return [1, 0];
+    }
+    if ($scorePlayer1 >= 21) {
+        return [0, 1];
+    }
+
+    $sums = [
+        0 => 0,
+        1 => 0,
+    ];
+
+    foreach ($GLOBALS['universeMultipliers'] as $posIncrement => $universeMultiplier) {
+        if ($turn % 2 === 0) {
+            $newPos = (($posPlayer0 - 1 + $posIncrement) % 10) + 1;
+            $newScore = $scorePlayer0 + $newPos;
+            $res = theForkingGame(
+                $newPos,
+                $newScore,
+                $posPlayer1,
+                $scorePlayer1,
+                $turn+1
+            );
+        } else {
+            $newPos = (($posPlayer1 - 1 + $posIncrement) % 10) + 1;
+            $newScore = $scorePlayer1 + $newPos;
+            $res = theForkingGame(
+                $posPlayer0,
+                $scorePlayer0,
+                $newPos,
+                $newScore,
+                $turn+1
+            );
         }
-    }
-    $myGamesWithScore = $newGamesWithScore;
 
-    // player 2  --  neither position nor score changes for player 2, but the number of universes multiplies!
-    // but only if player 1 didn't win with that move.
-    $newGamesWithScore2 = [];
-    foreach ($theirGamesWithScore as $posScore => $cnt) {
-        $newGamesWithScore2[$posScore] = $cnt * $openGames;
+        $sums[0] += $universeMultiplier * $res[0];
+        $sums[1] += $universeMultiplier * $res[1];
     }
-    $theirGamesWithScore = $newGamesWithScore2;
+
+    return $sums;
 }
 
-function debug($gamesWithScore1, $gamesWithScore2, $gamesWon1, $gamesWon2)
-{
-    echo "Player 1: won ($gamesWon1)\n";
-    print_r($gamesWithScore1);
-
-    echo "\nPlayer 2: won ($gamesWon2)\n";
-    print_r($gamesWithScore2);
-}
-
-debug($gamesWithScore1, $gamesWithScore2, $gamesWon1, $gamesWon2);
-
-$i = 0;
-while (count($gamesWithScore1) > 0 && count($gamesWithScore2) > 0) {
-    if ($i++ % 2 === 0) {
-        theForkingGame($gamesWithScore1, $gamesWithScore2, $gamesWon1);
-        echo $gamesWon1, ':', $gamesWon2, "\n";
-        continue;
-    }
-    theForkingGame($gamesWithScore2, $gamesWithScore1, $gamesWon2);
-    echo $gamesWon1, ':', $gamesWon2, "\n";
-}
-
-debug($gamesWithScore1, $gamesWithScore2, $gamesWon1, $gamesWon2);
-
+print_r(theForkingGame($posPlayer0, 0, $posPlayer1, 0, 0));
 
 echo "\ntotal time: ", (microtime(true) - $startTime), "\n";
 
