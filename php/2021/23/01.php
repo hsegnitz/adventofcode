a0c6786991b6565fb61f7bd80a20da5161679499
Holger Segnitz <holger@segnitz.net>
2021-12-26 12:46:21 +0100

    2021.23.1
    10611

--- php/2021/23/01.php
+++ php/2021/23/01.php
@@ -2,8 +2,9 @@
 
 $startTime = microtime(true);
 
-$input = '...........BACDBCDA';
-#$input = '...........DBCADABC';
+#$input = '...........BACDBCDA';
+#$input = '..........A.ABBCCDD';
+$input = '...........DBCADABC';
 
 const TARGET_STATE = '...........AABBCCDD';
 
@@ -26,14 +27,14 @@ const ROOMTOPS = [
     'A' => 11,
     'B' => 13,
     'C' => 15,
-    'D' => 16,
+    'D' => 17,
 ];
 
 const TARGETPOSITIONS = [
     'A' => [11, 12],
     'B' => [13, 14],
     'C' => [15, 16],
-    'D' => [16, 18],
+    'D' => [17, 18],
 ];
 
 function switchPositions(string $map, int $a, int $b): string    //a.k.a. do the move
@@ -49,14 +50,14 @@ function legalMovesFromState(string $state): Generator
     // move 'em down should they be in their room, on the top position and the bottom is empty
     foreach (ROOMTOPS as $type => $pos) {
         if ($state[$pos] === $type && $state[$pos+1] === '.') {
-            yield [switchPositions($state, $pos, $pos+1) => COSTS[$type]];
+            yield switchPositions($state, $pos, $pos+1) => COSTS[$type];
         }
     }
 
     // move 'em up should they not be in their room, and the top position is empty
     foreach (ROOMTOPS as $type => $pos) {
         if ($state[$pos+1] !== $type && $state[$pos+1] !== '.' && $state[$pos] === '.') {
-            yield [switchPositions($state, $pos, $pos+1) => COSTS[$state[$pos+1]]];
+            yield switchPositions($state, $pos, $pos+1) => COSTS[$state[$pos+1]];
         }
     }
 
@@ -71,7 +72,7 @@ function legalMovesFromState(string $state): Generator
         if ($state[$targetTile] !== '.') {  // topmost tile in target room occupied -- can't move
             continue;
         }
-        if ($state[$targetTile + 1] !== '.' && $state[$targetTile] !== $currentCritter ) { // bottom part in target room occupied by different race
+        if ($state[$targetTile + 1] !== '.' && $state[$targetTile + 1] !== $currentCritter ) { // bottom part in target room occupied by different race
             continue;
         }
 
@@ -79,12 +80,13 @@ function legalMovesFromState(string $state): Generator
         if ($entrance > $i) {
             $checkAgainst = str_pad('.', ($entrance - $i), '.');
             if (substr($state, $i+1, $entrance - $i) === $checkAgainst) {
-                yield [switchPositions($state, $i, $targetTile) => (COSTS[$currentCritter] * ($entrance - $i + 1))];
+                yield switchPositions($state, $i, $targetTile) => (COSTS[$currentCritter] * ($entrance - $i + 1));
             }
         } elseif ($entrance < $i) {
             $checkAgainst = str_pad('.', ($i - $entrance), '.');
-            if (substr($state, $entrance+1, $i - $entrance) === $checkAgainst) {
-                yield [switchPositions($state, $i, $targetTile) => (COSTS[$currentCritter] * ($i - $entrance + 1))];
+            $actual = substr($state, $entrance, $i - $entrance);
+            if ($actual === $checkAgainst) {
+                yield switchPositions($state, $i, $targetTile) => (COSTS[$currentCritter] * ($i - $entrance + 1));
             }
         }
 
@@ -116,22 +118,54 @@ function legalMovesFromState(string $state): Generator
             if (in_array($pos, ENTRANCES)) {
                 continue;
             }
-            yield [switchPositions($state, $position, $pos) => (COSTS[$currentCritter] * ($entrance - $pos + 1))];
+            yield switchPositions($state, $position, $pos) => (COSTS[$currentCritter] * ($entrance - $pos + 1));
         }
 
         //right
-        for ($pos = $entrance; $pos <= 11; $pos++) {
+        for ($pos = $entrance; $pos < 11; $pos++) {
             if ($state[$pos] !== '.') {  // topmost tile in target room occupied -- can't move
                 break;
             }
             if (in_array($pos, ENTRANCES)) {
                 continue;
             }
-            yield [switchPositions($state, $position, $pos) => (COSTS[$currentCritter] * ($pos - $entrance + 1))];
+            yield switchPositions($state, $position, $pos) => (COSTS[$currentCritter] * ($pos - $entrance + 1));
         }
     }
 }
 
+function out(string $state, int $cost): void
+{
+    echo 'Cost: ', $cost, "\n";
+    echo "#############\n";
+    echo "#", substr($state, 0, 11), "#\n";
+    echo "###{$state[11]}#{$state[13]}#{$state[15]}#{$state[17]}###\n";
+    echo "  #{$state[12]}#{$state[14]}#{$state[16]}#{$state[18]}#\n";
+    echo "  #########\n\n";
+}
+
+// list of shortest ways to achieve a state;
+$allStates = [
+    $input => 0,
+];
+
+$stack = [$input];
+
+while (count($stack)) {
+    $currentState = array_shift($stack);
+    $cost = $allStates[$currentState];
+    foreach (legalMovesFromState($currentState) as $nextMove => $addedCost) {
+        $totalCost = $cost + (int)$addedCost;
+        if (isset($allStates[$nextMove]) && $allStates[$nextMove] <= $totalCost) {
+            continue;
+        }
+        $allStates[$nextMove] = $totalCost;
+        $stack[] = $nextMove;
+    }
+    #echo count($stack), "\n";
+}
+
+echo $allStates[TARGET_STATE];
 
 
 echo "\ntotal time: ", (microtime(true) - $startTime), "\n";
