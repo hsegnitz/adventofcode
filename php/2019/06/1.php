a0cacbc2ffc078e32208288b8c3e2048d8d2ab06
Holger Segnitz <holger@segnitz.net>
2022-03-21 23:25:15 +0100

    2019.06.2
    1722

--- php/2019/06/1.php
+++ php/2019/06/1.php
@@ -34,6 +34,34 @@ foreach ($space as $so) {
     $sum += $so->countParents();
 }
 
-echo "Sum ", $sum, "\n";
+echo "Sum (Part 1)", $sum, "\n";
+
+function parentChain(SpaceObject $child): array {
+    $cur = $child;
+    $out = [];
+    while($cur->getName() !== "COM") {
+        $out[] = $cur->getName();
+        $cur = $cur->getParent();
+    }
+    $out[] = "COM";
+
+    return $out;
+}
+
+
+$me = $space["YOU"];
+$meAddress = parentChain($me);
+
+$santa = $space["SAN"];
+$santaAddress = parentChain($santa);
+
+$a = $b = '';
+
+while ($a === $b) {
+    $a = array_pop($meAddress);
+    $b = array_pop($santaAddress);
+}
+
+echo "Steps (Part 2): ", (count($meAddress) + count($santaAddress));
 
 echo "\ntotal time: ", number_format(microtime(true) - $startTime, 6), "\n";
