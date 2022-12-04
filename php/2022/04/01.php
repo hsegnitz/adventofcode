a0c71d03c3d302aca89289a21eba4918e3083b79
Holger Segnitz <holger@segnitz.net>
2022-12-04 09:15:02 +0100

    2022.04.2
    4640

--- php/2022/04/01.php
+++ php/2022/04/01.php
@@ -5,19 +5,20 @@ $startTime = microtime(true);
 #$input = file('./example.txt', FILE_IGNORE_NEW_LINES);
 $input = file('./in.txt', FILE_IGNORE_NEW_LINES);
 
-$count = 0;
+$count1 = $count2 = 0;
 foreach ($input as $line) {
     [$a, $b, $x, $y] = preg_split('/[,-]/', $line);
-    if ($a <= $x && $b >= $y) {
-        ++$count;
-        continue;
+    if (($a <= $x && $b >= $y) || ($a >= $x && $b <= $y)) {
+        ++$count1;
     }
-    if ($a >= $x && $b <= $y) {
-        ++$count;
+
+    if ((max($b, $y) - min($a, $x) + 1) < (($b - $a) + ($y - $x) + 2)) {
+        ++$count2;
     }
 }
 
-echo 'Part 1: ', $count;
+echo 'Part 1: ', $count1;
+echo "\nPart 2: ", $count2;
 
 echo "\ntotal time: ", (microtime(true) - $startTime), "\n";
 
